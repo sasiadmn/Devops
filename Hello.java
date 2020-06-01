ffc83446c6f5da66704ac5db1e7be3bb66f94cea
SASIMOHAN <ubuntu@ip-172-31-39-238.us-east-2.compute.internal>
2020-06-01 03:26:23 +0000

    added another print

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@
 class Hello{
 	public static void main(sting args[]{
 		System.out.println("HellO jAVA");
+		System.out.println("Welcome to Devops");
 	}
 }                                                                                                                                 
 
