5f1bb49d3b5672343f4adebd2e665bd080a7a6db
SASIMOHAN <ubuntu@ip-172-31-39-238.us-east-2.compute.internal>
2020-06-01 04:41:24 +0000

    added code part of feature1

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ class Hello{
 	public static void main(sting args[]{
 		System.out.println("HellO jAVA");
 		System.out.println("Welcome to Devops");
+		System.out.printlin("Welcome Feature1");
 	}
 }                                                                                                                                 
 
